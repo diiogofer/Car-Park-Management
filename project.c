e2b9aac9e5479c6fe462a5d39d5b730b92ae898f
Diogo Fernandes <diogo.sendim.fernandes@tecnico.ulisboa.pt>
2024-03-26 21:58:09 +0000

    main edit

--- project.c
+++ project.c
@@ -5,49 +5,31 @@
 #include <string.h>
 
 int main(){
+    /* Array of park pointers */
     Park *parkPtrArray[MAX_PARKS];
-    char buffer[MAX_BUFSIZ];
-    memset(parkPtrArray, 0, sizeof(parkPtrArray)); /* Pointers to NULL */
-	memset(buffer, '\0', MAX_BUFSIZ);
+    memset(parkPtrArray, 0, sizeof(parkPtrArray));
+    /* Buffer for user commands */
+    char buff[MAX_BUFSIZ];
+	memset(buff, '\0', MAX_BUFSIZ);
+    /* Initializing system with default date */
     Date date = {ZERO, ZERO, ZERO, ZERO, ZERO};
-    Sys system = sysCreator(buffer, parkPtrArray, 0, &date);
+    Sys system = sysCreator(buff, parkPtrArray, 0, &date);
+
+    /* Processing user commands */
     while (1){
-        if (fgets(buffer, MAX_BUFSIZ, stdin) == NULL || 
-            system.buffer[0] == 'q'){
+        if (fgets(buff, MAX_BUFSIZ, stdin) == NULL || system.buffer[0] == 'q') {
 			break;
 		}
 		switch (system.buffer[0]){
-            case 'p':
-                pCommand(&system);
-                break;
-            case 'e':
-                eCommand(&system);
-                    break;
-            
-            case 's':
-                sCommand(&system);
-                break;
-            
-            case 'v':
-                vCommand(&system);
-                break;
-            
-            case 'f':
-                fCommand(&system);
-                break;
-              
-            case 'r':
-                rCommand(&system);
-                break;     
-            case 'y':
-                printCarMatriculas(&system);
-                break;
-            
-            case 'z':
-                printMovMatriculas(&system);
-                break;
+            case 'p': pCommand(&system); break;
+            case 'e': eCommand(&system); break;
+            case 's': sCommand(&system); break;
+            case 'v': vCommand(&system); break;
+            case 'f': fCommand(&system); break;
+            case 'r': rCommand(&system); break;     
         }
-        memset(buffer, '\0', MAX_BUFSIZ);
+    /* Freeing allocated memory for parks */
+        memset(buff, '\0', MAX_BUFSIZ);
 	}
     freeAllParks(&system);
     return 0;
